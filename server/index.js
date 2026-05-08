066f1ea94109499dc470b9cd54cf2b913c3e8c74
zenvix50 <zenvix50@gmail.com>
2026-05-08 21:30:03 +0530

    fix index merge conflict

--- server/index.js
+++ server/index.js
@@ -3,12 +3,10 @@ import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
-<<<<<<< HEAD
-=======
+
 import path from 'path';
 import { fileURLToPath } from 'url';
 
->>>>>>> 1803d81d78bf4c490906779263f193a00f186b63
 import connectDB from './config/db.js';
 
 import releasesRouter from './routes/releases.js';
@@ -19,22 +17,15 @@ import errorHandler from './middleware/errorHandler.js';
 
 dotenv.config();
 
-<<<<<<< HEAD
-// Connect MongoDB
-=======
 // MongoDB Connect
->>>>>>> 1803d81d78bf4c490906779263f193a00f186b63
 connectDB();
 
 const app = express();
 
-<<<<<<< HEAD
-=======
 // __dirname Fix for ES Modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
->>>>>>> 1803d81d78bf4c490906779263f193a00f186b63
 // Middlewares
 app.use(helmet());
 
@@ -46,27 +37,16 @@ app.use(cors({
 app.use(morgan('dev'));
 app.use(express.json());
 
-<<<<<<< HEAD
-// Test Route
-app.get('/', (req, res) => {
-  res.send('ZenviX API Running');
-});
-
-// Routes
-=======
 // API Test Route
 app.get('/api', (req, res) => {
   res.send('ZenviX API Running');
 });
 
 // API Routes
->>>>>>> 1803d81d78bf4c490906779263f193a00f186b63
 app.use('/api/releases', releasesRouter);
 app.use('/api/downloads', downloadsRouter);
 app.use('/api/subscribe', subscribeRouter);
 
-<<<<<<< HEAD
-=======
 // ================================
 // FRONTEND BUILD FILES
 // ================================
@@ -80,7 +60,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(clientPath, 'index.html'));
 });
 
->>>>>>> 1803d81d78bf4c490906779263f193a00f186b63
 // Error Middleware
 app.use(errorHandler);
 
@@ -95,8 +74,4 @@ const server = app.listen(PORT, () => {
 // Error Handling
 server.on('error', (err) => {
   console.error('Server Error:', err);
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> 1803d81d78bf4c490906779263f193a00f186b63
